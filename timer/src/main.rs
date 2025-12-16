c4777dbb91601a7acb97b983467aba526f2936f5
Tony Bierman <tonybierman@gmail.com>
2025-12-16 06:02:26 -0600

    Working on build workflows

--- timer/src/main.rs
+++ timer/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use iced::alignment::Vertical;
+use iced::widget::{button, column, container, row, text};
+use iced::{Element, Settings};
+
+pub fn main() -> iced::Result {
+    iced::application(App::new, App::update, App::view)
+        .settings(Settings {
+            antialiasing: true,
+            ..Default::default()
+        })
+        .window_size((200, 80))
+        .centered()
+        .resizable(false)
+        .run()
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+}
+
+#[derive(Default)]
+struct App {
 }
