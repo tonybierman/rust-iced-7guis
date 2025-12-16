0a04b9159b162106aafb04301bbcf5cbfd8a8bb0
Tony Bierman <tonybierman@gmail.com>
2025-12-16 06:29:15 -0600

    Working on timer

--- timer/src/main.rs
+++ timer/src/main.rs
@@ -1,9 +1,11 @@
 use iced::alignment::Vertical;
+use iced::time::{self, milliseconds};
 use iced::widget::{button, column, container, row, text};
-use iced::{Element, Settings};
+use iced::{Element, Settings, Subscription} ;
 
 pub fn main() -> iced::Result {
     iced::application(App::new, App::update, App::view)
+        .subscription(App::subscription)
         .settings(Settings {
             antialiasing: true,
             ..Default::default()
@@ -16,8 +18,42 @@ pub fn main() -> iced::Result {
 
 #[derive(Debug, Clone, Copy)]
 enum Message {
+    Increment,
 }
 
 #[derive(Default)]
 struct App {
+    value: f32,
+}
+
+impl App {
+    fn new() -> (Self, iced::Task<Message>) {
+        (App { value: 0.0 }, iced::Task::none())
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Increment => {
+                self.value += 0.05;
+            }
+        }
+    }
+
+    fn view(&self) -> Element<'_, Message> {
+
+        container(
+            column![
+                text(format!("{:.2}", self.value))
+                    .size(24)
+            ]
+            .padding(20),
+        )
+        .center_x(iced::Length::Fill)
+        .into()
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        time::every(milliseconds(50)).map(|_| Message::Increment)
+    }
+
 }
